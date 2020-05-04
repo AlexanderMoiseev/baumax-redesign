40bd98bbb26f1db09d1b09c492bdc826ef85e7e9
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2020-05-04 19:50:14 +0300

    styling

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 console.log('Hello from js');
 
+// (function() {
+//     var $contactForm = $('#contactForm');
+//     // VALIDATE FORM
+//     $contactForm.validate();
+//     $contactForm.submit(function(e) {
+//         if ($(this).valid()) {
+//             return true;
+//         }
+//        return false;
+//      });
+// })();
