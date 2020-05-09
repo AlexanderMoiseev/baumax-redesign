07037c61bab523e8e1d74cc463fd4cbdda62539b
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2020-05-09 04:15:45 +0300

    mask for phone

--- index.js
+++ index.js
@@ -1,104 +1,35 @@
-console.log('Hello from js');
-
-
-
-
-
-/** SITE JS CODE **/
-
 $(document).ready(function() {
-
-
-    setTimeout(function(){
-      $('body').addClass('loaded');
-    }, 7000);
-  
     /*******************************
     **** FULL SCREEN NAVIGATION ****
     ********************************/
   
     $('#menu-toggle').click(function() {
       $(this).toggleClass('active');
-      $('.logo').toggleClass('logo-menu-open');
       $('.menu-overlay').toggleClass('open');
     });
   
     $('.main-nav li a').click(function() {
       $('#menu-toggle').removeClass('active');
-      $('.logo').removeClass('logo-menu-open');
       $('.menu-overlay').removeClass('open');
     });
   
     $('a[href="#top"]').click(function() {
       $('#menu-toggle').removeClass('active');
-      $('.logo').removeClass('logo-menu-open');
       $('.menu-overlay').removeClass('open');
     });
-  
-  
-    /***********************
-    *** STICKY NAVIGATION **
-    ************************/
-  
-    // $('.js-section-profile').waypoint(function(direction) {
-    //   if (direction === 'down') {
-    //     //fadeout hide the nav
-    //     // nav must be fadeOut first in order to fadeIn
-    //     $('nav').fadeOut("fast", function() {
-    //       // then add the sticky class to the nav while is hidden
-    //       $('nav').addClass('sticky');
-    //     });
-    //     //show the nav again with the class sticky already added
-    //     $('nav').fadeIn("slow");
-  
-    //   } else {
-    //     //fadeout the nav again
-    //     $('nav').fadeOut("slow", function() {
-    //       //remove the sticky class
-    //       $('nav').removeClass('sticky');
-    //     });
-    //     $('nav').fadeIn();
-    //   }
-    // }, {
-    //   offset: '0;'
-    // });
-  
-  
-    /***************************************
-    *** SCROLL ON HEADER SCROLLING BUTTON **
-    ***************************************/
-  
-    //-- go to section plans when click on Start training now button
-    // $('.js-scroll-to-profile').click(function() {
-    //   $('html, body').animate(
-    //     {
-    //       scrollTop: $('.js-section-profile').offset().top
-    //     },
-    //     {
-    //       duration: 1000,
-    //       easing: 'linear'
-    //     })
-    // });
-  
-    //-- go to footer when click on let's talk nav link
-    // $('a[href="#contact"]').click(function() {
-    //   $('html, body').animate(
-    //     {
-    //       scrollTop: $('html, body').height()
-    //     },
-    //     {
-    //       duration: 1000,
-    //       easing: 'linear'
-    //     })
-    // });
-  
+ 
   
     /**************************
     *** SCROLL ON NAVIGATION **
     **************************/
     /* https://css-tricks.com/snippets/jquery/smooth-scrolling */
-  
+   
       $(function() {
+        $("#phone").mask("+7 (999) 999-9999", { completed:function() {
+          // $(".send-button").removeAttr( "disabled", "disabled" );
+        }});
+        $(".send-button").attr( "disabled", "disabled" );
+        
         $('a[href*="#"]:not([href="#"],  [href*="#modal-project"])').click(function() {
           if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
             var target = $(this.hash);
@@ -152,7 +83,6 @@ $(document).ready(function() {
       }, {
         offset: '200px;'
       });
-  
   
       // expeience section animations
       $('.js-section-experience').waypoint(function(direction) {
